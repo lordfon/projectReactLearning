0ce65418680c343b8cdb038577b1baec75f71ee5
mikhail <stratan.eng@gmail.com>
2021-03-18 22:54:54 +0200

    second stage - movieDB object

--- First Task/script.js
+++ First Task/script.js
@@ -1,3 +1,12 @@
 "use stict";
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
