b58c60f171acc39832c1e4d6097bd58f07186d45
mikhail <stratan.eng@gmail.com>
2021-03-19 23:04:57 +0200

    cycle for questions to user

--- First Task/script.js
+++ First Task/script.js
@@ -11,12 +11,35 @@ const personalMovieDB = {
 
 };
 
-let a = prompt("Один из последних просмотренных фильмов?"),
-    b = prompt("На сколько оцените его?"),
-    c = prompt("Один из последних просмотренных фильмов?"),
-    d = prompt("На сколько оцените его?");
+let i = 0;
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+// do {
+//     const a = prompt("Один из последних просмотренных фильмов?"),
+//         b = prompt("На сколько оцените его?");
+//     personalMovieDB.movies[a]=b;
+//     i++;
+// } while (i < 2);
+
+// i = 0;
+
+while (i < 3) {
+    let a = prompt("Один из последних просмотренных фильмов?"),
+        b = prompt("На сколько оцените его?");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a]=b;
+        console.log("well done!");        
+    } else {
+        console.log("error");
+        i--;
+    }
+
+    i++;
+}
+
+// for (let j = 0; j < 3; j++ ) {
+//     let a = prompt("Один из последних просмотренных фильмов?"),
+//         b = prompt("На сколько оцените его?");
+//     personalMovieDB.movies[a]=b;
+// }
 
 console.log(personalMovieDB.movies);
